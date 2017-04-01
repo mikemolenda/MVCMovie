7aa24f91e57bcee305e87394d90be6a13796456a
Mike Molenda <molenda.mike@gmail.com>
2017-04-01 12:04:23 -0500

    Add utility class for connecting to MySQL database

---
+++ src/com/mikemolenda/mvcmovie/utils/MySQLConnUtils.java
@@ -0,0 +1,47 @@
+package com.mikemolenda.mvcmovie.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Static methods to establish connections to a MySQL database
+ */
+public class MySQLConnUtils {
+
+    /**
+     * Establishes new MySQL connection using the default parameters
+     * @return Connection - MySQL connection object
+     * @throws ClassNotFoundException
+     * @throws SQLException
+     */
+    public static Connection getMySQLConnection()
+            throws ClassNotFoundException, SQLException {
+
+        String hostname = "localhost";
+        String dbName = "mvcmovie";
+        String username = "root";
+        String password = "WX33mqbzCTkBmn$M";
+
+        return getMySQLConnection(hostname, dbName, username, password);
+
+    }
+
+    /**
+     * Establishes new MySQL connection using supplied parameters
+     * @return Connection - MySQL connection object
+     * @throws ClassNotFoundException
+     * @throws SQLException
+     */
+    public static Connection getMySQLConnection(
+            String hostname, String dbName, String username, String password)
+            throws ClassNotFoundException, SQLException {
+
+        String connectionUrl = "jdbc:mysql://" + hostname + ":3306/" + dbName;
+
+        Connection conn = DriverManager.getConnection(connectionUrl, username, password);
+        return conn;
+
+    }
+
+}
